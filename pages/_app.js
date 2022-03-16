8653f6d4c39dbd0901f7ae467f1712c1ca15c705
HirasParasian <hirasparasian@gmail.com>
2022-03-16 13:39:25 +0700

    feat : init

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import '../styles/globals.css'
+import '../styles/application.scss'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ export default function Home() {
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
-        <p className={styles.description}>
+        <p className="bg-color1">
           Get started by editing{' '}
           <code className={styles.code}>pages/index.js</code>
         </p>

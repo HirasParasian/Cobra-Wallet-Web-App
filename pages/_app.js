fdf081af1dae6f363bfb8db091d726d3da82d6fb
HirasParasian <hirasparasian@gmail.com>
2022-03-16 16:21:47 +0700

    Feat : Login

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/globals.css'
 import '../styles/application.scss'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '@popperjs/core'
+import { useEffect } from 'react'
+
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    import("bootstrap/dist/js/bootstrap");
+  }, []);
+
   return <Component {...pageProps} />
 }
 

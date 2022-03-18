068645e977e2ecd353ca795c5c06a209c2797196
HirasParasian <hirasparasian@gmail.com>
2022-03-18 13:36:36 +0700

    Feat : Redux

---
+++ helpers/http.js
@@ -0,0 +1,29 @@
+import { default as axios } from 'axios'
+
+export const getData = async (url, history) => {
+	try {
+		const data = await axios.get(url)
+		return data
+	} catch (e) {
+		if (e.message.includes('404')) {
+			history.push('/404')
+		}
+	}
+}
+const { REACT_APP_BACKEND_URL } = process.env
+
+const http = (token, useUpload) => {
+    const headers = {}
+    if (token) {
+        headers['Authorization'] = `Bearer ${token}`
+    }
+    if (useUpload) {
+      headers['Content-Type'] = 'multipart/form-data'
+    }
+    return axios.create({
+        baseURL: REACT_APP_BACKEND_URL,
+        headers
+    })
+
+}
+export default (axios, http)

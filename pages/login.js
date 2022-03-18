78ac46c583390c132bebb86f9444553be00baf25
HirasParasian <hirasparasian@gmail.com>
2022-03-18 14:36:44 +0700

    Feat : Auth Login

--- pages/login.js
+++ pages/login.js
@@ -1,21 +1,33 @@
 import React from 'react'
-import { Row, Col, Container } from 'react-bootstrap'
+import { Row, Col, Container, Form } from 'react-bootstrap'
 import Input from '@mui/material/Input';
-import Button from '@mui/material/Button';
+import Button from 'react-bootstrap/Button'
 import InputAdornment from '@mui/material/InputAdornment';
 import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
 import LockIcon from '@mui/icons-material/Lock';
 import Link from 'next/link'
+import { useDispatch, useSelector } from 'react-redux'
+import { login } from '../redux/actions/auth'
 
-const login = () => {
+
+const Login = () => {
+    const auth = useSelector(state => state.auth)
+    const dispatch = useDispatch()
+
+    const onLogin = (event) => {
+        event.preventDefault()
+        const email = event.target.elements['email'].value
+        const password = event.target.elements['password'].value
+        dispatch(login(email, password))
+    }
     return (
         <>
-            <headers>
-                <Container>
-                    <Row>
-                        <Col sm={12} md={7} className="bg-color2">
-                        </Col>
-                        <Col sm={12} md={5} className="d-grid ">
+            <Container>
+                <Row>
+                    <Col sm={12} md={7} className="bg-color2">
+                    </Col>
+                    <Col sm={12} md={5} className="d-grid ">
+                        <Form onSubmit={onLogin}>
                             <h2 className='mx-4 my-5'>Start Accessing Banking Needs
                                 With All Devices and All Platforms
                                 With 30.000+ Users
@@ -24,8 +36,8 @@ const login = () => {
                                 Transfering money is eassier than ever, you can access Zwallet wherever you are.
                                 Desktop, laptop, mobile phone? we cover all of that for you!
                             </p>
-                            <Input type="email" placeholder='Email ..' className='py-3 my-2 mx-4' startAdornment={<InputAdornment position="start"><EmailOutlinedIcon /></InputAdornment>} />
-                            <Input type='password' placeholder='Password ..' className='py-3 mx-4' startAdornment={<InputAdornment position="start"><LockIcon /></InputAdornment>} />
+                            <Input name="email" type="email" placeholder='Email ..' className='py-3 my-2 mx-4' startAdornment={<InputAdornment position="start"><EmailOutlinedIcon /></InputAdornment>} />
+                            <Input name="password" type='password' placeholder='Password ..' className='py-3 mx-4' startAdornment={<InputAdornment position="start"><LockIcon /></InputAdornment>} />
                             <div className='mx-4 my-4'>
                                 <Link href='/forgotpassword' >
                                     <a className="d-flex justify-content-end text-decoration-none text-color3">
@@ -33,25 +45,25 @@ const login = () => {
                                     </a>
                                 </Link>
                             </div>
-                            <Button className='mx-4 py-3 my-5 rounded-btn bg-color3' variant="contained" >
-                                Delete
+                            <Button type="submit" className='mx-4 py-3 my-5 rounded-btn bg-color3' variant="contained" >
+                                Log In
                             </Button>
-                            <div className="d-flex justify-content-center my-5">
-                                <p>
-                                    Don’t have an account? Let’s &nbsp;
-                                </p>
-                                <Link href='/signup'>
-                                    <a className="d-flex justify-content-end text-decoration-none text-color3">
-                                        Sign Up
-                                    </a>
-                                </Link>
-                            </div>
-                        </Col>
-                    </Row>
-                </Container>
-            </headers>
+                        </Form>
+                        <div className="d-flex justify-content-center my-5">
+                            <p>
+                                Don’t have an account? Let’s &nbsp;
+                            </p>
+                            <Link href='/signup'>
+                                <a className="d-flex justify-content-end text-decoration-none text-color3">
+                                    Sign Up
+                                </a>
+                            </Link>
+                        </div>
 
+                    </Col>
+                </Row>
+            </Container>
         </>
     )
 }
-export default login
+export default Login
---
+++ redux/reducers/auth.js
@@ -0,0 +1,80 @@
+const initialState = {
+    token: null,
+    userData: {},
+    isLoading: false,
+    isError: false,
+    errorMsg: '',
+    errMsg: '',
+    successMsg: ''
+}
+
+const auth = (state = initialState, action) => {
+    switch (action.type) {
+        case 'AUTH_LOGIN_PENDING': {
+            state.isLoading = true
+            state.isError = false
+            state.errorMsg = ''
+            return { ...state }
+        }
+        case 'AUTH_LOGIN_FULFILLED': {
+            const { data } = action.payload
+            state.isLoading = false
+            state.isError = false
+            state.token = data.result
+            state.errorMsg = ""
+            state.errMsg = ""
+            if (!window.localStorage.getItem('token')) {
+                window.localStorage.setItem('token', state.token)
+            }
+            return { ...state }
+        }
+        case 'AUTH_LOGIN_REJECTED': {
+            const { data } = action.payload.response
+            state.isLoading = false
+            state.isError = true
+            state.errorMsg = data.error
+            state.errMsg = data.message
+            return { ...state }
+        }
+        case 'AUTH_LOGOUT': {
+            state.token = null
+            state.userData = {}
+            window.localStorage.removeItem('token')
+            return { ...state }
+        }
+        case 'SET_EMAIL': {
+            state.email = action.payload.email
+            return { ...state }
+        }
+        case 'GET_PROFILE_PENDING': {
+            state.error = false
+            state.isLoading = true
+            return { ...state }
+        }
+        case 'GET_PROFILE_FULFILLED': {
+            const { data } = action.payload
+            state.isLoading = false
+            state.userData = data.result
+            return { ...state }
+
+        }
+        case 'GET_PROFILE_REJECTED': {
+            const { data } = action.payload.response
+            state.isLoading = false
+            state.error = true
+            state.errorMsg = data.message
+            return { ...state }
+        }
+        case 'CLEAR_MESSAGE': {
+            state.errorMsg = ""
+            state.errMsg = ""
+            state.message = ""
+            return { ...state }
+        }
+        default: {
+            return { ...state }
+        }
+    }
+}
+
+export default auth

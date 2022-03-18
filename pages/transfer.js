e6daec93d70b89aaa11fdd60c8578413b221676d
HirasParasian <hirasparasian@gmail.com>
2022-03-18 10:51:03 +0700

    Feat : Transfer Pages

--- pages/transfer.js
+++ pages/transfer.js
@@ -2,6 +2,9 @@
 import { Container, Row, Col } from 'react-bootstrap'
 import SideBar from '../components/SideBar'
 import Navbar from '../components/Navbar'
+import CardTransfer from '../components/CardTransfer'
+import photo from "../images/navimg.png"
+
 
 
 const transfer = () => {
@@ -10,13 +13,19 @@ const transfer = () => {
             <Navbar />
             <Container className='my-5'>
                 <Row>
-                    <Col xs={3}>
+                    <Col sm={12} md={3}>
                         <SideBar />
                     </Col>
                     <Col>
                         <Row>
-                            <Col xs={12}>
-
+                            <Col xs={12} className="shadow rounded-btn2">
+                                <div>
+                                    <h4>Search Receiver</h4>
+                                </div>
+                                <CardTransfer name={"Hiras Parasian"} photo={photo} phoneNumber={"+62 813-8898-1122"} />
+                                <CardTransfer name={"Hiras Parasian"} photo={photo} phoneNumber={"+62 813-8898-1122"} />
+                                <CardTransfer name={"Hiras Parasian"} photo={photo} phoneNumber={"+62 813-8898-1122"} />
+                                <CardTransfer name={"Hiras Parasian"} photo={photo} phoneNumber={"+62 813-8898-1122"} />
                             </Col>
                         </Row>
                     </Col>

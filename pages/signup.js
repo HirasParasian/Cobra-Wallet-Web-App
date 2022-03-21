fbb6007721094c7d603021f0fd51e0db22886315
HirasParasian <hirasparasian@gmail.com>
2022-03-21 11:13:55 +0700

    update : signup

--- pages/signup.js
+++ pages/signup.js
@@ -7,9 +7,11 @@ import { FiMail } from 'react-icons/fi'
 import { FaLock } from 'react-icons/fa'
 import ModalSuccess from '../components/ModalSuccess'
 import ModalError from '../components/ModalError'
+import ModalLoading from '../components/ModalLoading'
 import { DataRegister } from '../redux/actions/signup'
 import { useDispatch, useSelector } from 'react-redux'
 import { useRouter } from "next/router"
+
 
 
 
@@ -28,6 +30,7 @@ const Signup = () => {
             password: e.target.elements['password'].value,
         }
         dispatch(DataRegister(data))
+        dispatch({ type: 'CLEAR_MESSAGE' });
         router.push('/pin')
     }
 
@@ -36,54 +39,53 @@ const Signup = () => {
         <>
             {/* <ModalSuccess message={auth.successMsg} />
             <ModalError message={auth.errorMsg} /> */}
-            <headers>
-                <Container>
-                    <Row>
-                        <Col sm={12} md={7} className="bg-color2">
-                        </Col>
-                        <Col sm={12} md={5} className="d-grid ">
-                            <h2 className='mx-4 my-3'>Start Accessing Banking Needs
-                                With All Devices and All Platforms
-                                With 30.000+ Users
-                            </h2>
-                            <p className='mx-4 my-3'>
-                                Transfering money is eassier than ever, you can access Zwallet wherever you are.
-                                Desktop, laptop, mobile phone? we cover all of that for you!
-                            </p>
-                            <Form className='mx-4' onSubmit={handleSubmit}>
-                                <div className="d-grid gap-2">
-                                    <Input type="text" name="firstName" placeholder='First Name ..' className='py-3 my-2 mx-5' icon={<FiMail />} />
-                                    <Input type="text" name='lastName' placeholder='Last Name ..' className='py-3 mx-4' icon={<FaLock />} />
-                                    <Input name="email" type="email" placeholder='Email ..' className='py-3 my-2 mx-5' icon={<FiMail />} />
-                                    <Input name="password" type='password' placeholder='Password ..' className='py-3 mx-4' icon={<FaLock />} />
-                                </div>
-                                <div className='mx-4 my-4'>
-                                    <Link href='/forgotpassword' >
-                                        <a className="d-flex justify-content-end text-decoration-none text-color1">
-                                            Forgot Password?
-                                        </a>
-                                    </Link>
-                                </div>
-                                <div className="d-grid gap-2">
-                                    <Button type="submit" variant="color3" className='btn py-3 my-5 rounded-btn2 bg-color1 text-light btn-color1' size="lg">
-                                        Log In
-                                    </Button>
-                                </div>
-                            </Form>
-                            <div className="d-flex justify-content-center my-5">
-                                <p>
-                                    Don’t have an account? Let’s &nbsp;
-                                </p>
-                                <Link href='/signup'>
-                                    <a className="d-flex justify-content-end text-decoration-none text-color3">
-                                        Sign Up
+            <Container>
+                <Row>
+                    <Col sm={12} md={7} className="bg-color2">
+                    </Col>
+                    <Col sm={12} md={5} className="d-grid ">
+                        <h2 className='mx-4 my-3'>Start Accessing Banking Needs
+                            With All Devices and All Platforms
+                            With 30.000+ Users
+                        </h2>
+                        <p className='mx-4 my-3'>
+                            Transfering money is eassier than ever, you can access Zwallet wherever you are.
+                            Desktop, laptop, mobile phone? we cover all of that for you!
+                        </p>
+                        <Form className='mx-4' onSubmit={handleSubmit}>
+                            <div className="d-grid gap-2">
+                                <Input type="text" name="firstName" placeholder='First Name ..' className='py-3 my-2 mx-5' icon={<FiMail />} />
+                                <Input type="text" name='lastName' placeholder='Last Name ..' className='py-3 mx-4' icon={<FaLock />} />
+                                <Input name="email" type="email" placeholder='Email ..' className='py-3 my-2 mx-5' icon={<FiMail />} />
+                                <Input name="password" type='password' placeholder='Password ..' className='py-3 mx-4' icon={<FaLock />} />
+                            </div>
+                            <div className='mx-4 my-4'>
+                                <Link href='/forgotpassword' >
+                                    <a className="d-flex justify-content-end text-decoration-none text-color1">
+                                        Forgot Password?
                                     </a>
                                 </Link>
                             </div>
-                        </Col>
-                    </Row>
-                </Container>
-            </headers>
+                            <div className="d-grid gap-2">
+                                <Button type="submit" variant="color3" className='btn py-3 my-5 rounded-btn2 bg-color1 text-light btn-color1' size="lg">
+                                    Sign Up
+                                </Button>
+                            </div>
+                        </Form>
+                        <div className="d-flex justify-content-center my-5">
+                            <p>
+                                have an account? Let’s &nbsp;
+                            </p>
+                            <Link href='/login'>
+                                <a className="d-flex justify-content-end text-decoration-none text-color3">
+                                    Log In
+                                </a>
+                            </Link>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+
 
         </>
     )
